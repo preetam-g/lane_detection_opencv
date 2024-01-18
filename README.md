7e6caaaafd82284d99721af6a0a42943c8f58430
Preetam Gopalasetty <preetamgopalasetty@gmail.com>
2024-01-18 21:53:17 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# lane_detection_opencv
+# Lane Detection using OpenCV in python
 A lane detection program using openCV in python. 
 The region of interest has been selected has the bottom part of the image and then lines have been plotted. This has been to make sure that the program works for wide range of vidoes. 
+This program also prints the houghlines array and its to type to show that in some cases we may have nonetype. To avoid errors an if statement has been added to check the type of houghlines array.
+
+# Test Videos
+Three test videos have been given to check the working of the program. This program may not be suitable for all types of videos but if you tune the thresholds in canny, matrix size in guassian blur, then you can use it according to your requirement. 
+
+# How to use
+Make sure the video file you want to test and the program file are in the same directory then use the terminal to change your working directory to the program directory. Then change the name "Video1.mp4" in cv.VideoCapture() with your video file in the main.py file. Then you can type "python main.py" in VSCode, "python3 main.py" in Ubuntu in the terminal to run the program. 
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 import numpy as np 
 
-num = int(input("give video num you want to test on(1,2,3): "))
-cap = cv.VideoCapture(f"Video{num}.mp4")
+cap = cv.VideoCapture(f"Video1.mp4") # name of the video file you want to detect lines on
 
 while cap.isOpened():
 
@@ -11,32 +10,33 @@ while cap.isOpened():
 
     if success:
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # converting frame into grayscale
         # cv.imshow("gray", gray)
 
-        blur = cv.GaussianBlur(gray, (7,7), 0)
+        blur = cv.GaussianBlur(gray, (7,7), 0) # blurring the image to reduce noise
         # cv.imshow("blur", blur)
 
-        canny = cv.Canny(blur, 100, 200)
+        canny = cv.Canny(blur, 100, 200) # detect edges using canny
         # cv.imshow("canny", canny)
 
-        height, width = canny.shape
+        height, width = canny.shape # create a region of interest, in this case it is the bottom part of the image
         roi = canny[int(0.3*height): , :]
 
-        
+        # apply houghlines in the roi 
         hough_lines = cv.HoughLinesP(roi, 1, np.pi/180, 200, minLineLength = 40, maxLineGap = 10)
         print((hough_lines), type(hough_lines))
-        
-        if type(hough_lines) == np.ndarray:
-            for line in hough_lines:
+
+
+        if type(hough_lines) == np.ndarray: # to make sure that we have hough_lines, sometimes we get nonetype
+            for line in hough_lines: # drawing lines on the bottom part of actual frame because roi was bottom part 
                 x, y, w, h = line[0]
                 res = cv.line(frame[int(0.3*height):, :], (x, y), (w, h), color = (120, 250, 0), thickness = 4)
         else: pass
         
-        cv.imshow("hi there", res)
+        cv.imshow("hi there", res) # displaying the result
 
-        if cv.waitKey(40) == ord('q'): break
-    else: cv.waitKey(1000); break
+        if cv.waitKey(40) == ord('q'): break # if you press q during the run then exit
+    else: cv.waitKey(1000); break # wait 1 sec when the whole video is read and then exit 
 
 
 cv.destroyAllWindows()
